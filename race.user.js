ba4f828328b56e665271828bce77c0b6cdc1c305
Prabhakar Rai <student343.bhu@gmail.com>
2020-05-23 01:57:46 +0530

    Create race.user.js

---
+++ race.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Nitro Typer 5.0
+// @namespace    https://www.theprabhakar.in/
+// @version      5.0
+// @description  Browser counterpart of Nitro Typer 5.0
+// @author       Prabhakar Rai
+// @match        https://www.nitrotype.com/race/*
+// @match        https://www.nitrotype.com/race
+// @icon         https://raw.githubusercontent.com/PrabhakarRai/nitro-type-hack-bot/master/icon.png
+// @updateURL    https://raw.githubusercontent.com/PrabhakarRai/nitro-type-hack-bot/master/race.user.js
+// @downloadURL  https://raw.githubusercontent.com/PrabhakarRai/nitro-type-hack-bot/master/race.user.js
+// @require https://code.jquery.com/jquery-3.5.1.min.js
+// @grant unsafeWindow
+// @grant GM_addStyle
+// @grant GM_getValue
+// @grant GM_setValue
+// @grant GM_registerMenuCommand
+// @grant GM_listValues
+// @grant GM_getResourceText
+// @grant GM_getResourceURL
+// @grant GM_log
+// @grant GM_setClipboard
+// @grant GM_info
+// ==/UserScript==
+
+//To be released on 26/05/20
